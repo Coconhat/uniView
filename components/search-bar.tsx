6c03b2594ca2c1bac104db81b9e2e8f01fc2b126
coconhat <nhatvu2669@gmail.com>
2025-04-07 11:51:03 +0800

    style: enhance search input style

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useUniversities } from "@/hooks/use-universities";
 import { PlaceholdersAndVanishInput } from "../components/ui/placeholders-and-vanish-input";
+import { inter } from "@/app/fonts";
 
 export function SearchInput() {
   const placeholders = [
@@ -11,6 +13,8 @@ export function SearchInput() {
     "Find your college",
   ];
 
+  const { university, loading, error } = useUniversities();
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
   };
@@ -20,7 +24,9 @@ export function SearchInput() {
   };
   return (
     <div className="h-[10rem] mb-10 flex flex-col justify-center  items-center px-4">
-      <h2 className="mb-7 sm:mb-15 text-xl text-center sm:text-5xl  text-white">
+      <h2
+        className={`mb-7 sm:mb-15 text-3xl text-center sm:text-5xl font-light text-white ${inter.className}`}
+      >
         Search for a university
       </h2>
       <PlaceholdersAndVanishInput
